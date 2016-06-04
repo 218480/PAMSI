6bae8fa88588e398c022ac7c5463080a94daa4ef
Grzesiek <218480@student.pwr.edu.pl>
2016-06-04 06:32:34 -0700

    poprawa B&B

--- Lab8/inc/bnb.h
+++ Lab8/inc/bnb.h
@@ -1,16 +1,13 @@
 #ifndef guard_bnb
 #define guard_bnb
 #include "../inc/wgraph.h"
-#include "../inc/path.h"
+#include "../inc/queue.h"
 class bnb
 {
-private:
-  int dist=1;
-  stack best;
-  bool browse(wgraph, stack&, int, int, int);
 public:
 int lenght,edges=0;
-  stack search(wgraph, int, int);
+int search(wgraph, int, int);
+int esearch(wgraph, int, int);
 };
 
 
--- Lab8/src/pomiardrogi.cpp
+++ Lab8/src/pomiardrogi.cpp
@@ -1,29 +1,28 @@
 #include "../inc/stoper.h"
 #include "../inc/pomiardrogi.h"
-#include "../inc/stack.h"
 #include "../inc/wgraph.h"
 #include "../inc/bnb.h"
-#include "../inc/bnbe.h"
 #include <iostream>
 
   double pomiardrogi::wykonaj_pomiar(int ilosc){
-stack s1,s2;
-wgraph g1;
 stoper t1;
+wgraph g1;
 bnb b1;
-bnbe e1;
-int i,b1e=0,be1e=0;/*
-for(i=0;i<ilosc;i++){
-g1.add_vertex();
-if(i>0) g1.add_edge(i,i-1);
-}*/
+int i=0, total=0;
+srand(time(NULL));
 g1.test(ilosc);
+for(int i=0;i<ilosc*10;i++)
+g1.add_edge(rand() % ilosc, rand() % ilosc,rand() % 10);
 t1.start();
-std::cout<<ilosc<<" elementow:"<<std::endl;/*
-for(i=0;i<10;i++){s1=b1.search(g1,0,ilosc-1);b1e+=b1.edges;}
-std::cout<<"czas dla bnb: "<<t1.stop()/10<<", krawedzi: "<<b1e/10<<std::endl;
-t1.start();*/
-for(i=0;i<10;i++){s2=e1.search(g1,0,ilosc-1);be1e+=e1.edges;}
-std::cout<<"czas dla bnbe: "<<t1.stop()/10<<", krawedzi: "<<be1e/10<<std::endl;
+for(i=0;i<10;i++) total+=b1.search(g1,0,ilosc-1);
+std::cout<< "average bnb edges checked for size "<< ilosc <<" : "<<total/10<<std::endl;
+std::cout<<"in time: "<<t1.stop()/10<<" ms"<<std::endl;
+total=0;
+t1.start();
+for(i=0;i<10;i++) total+=b1.esearch(g1,0,ilosc-1);
+std::cout<< "average bnbe edges checked for size "<< ilosc <<" : "<<total/10<<std::endl;
+std::cout<<"in time: "<<t1.stop()/10<<" ms"<<std::endl;
+
+
     return t1.stop();
 }
